014df3f3612cb42449624c461f63f3109b03f5e1
Ckonstant <125656019+Ckonstant@users.noreply.github.com>
2023-03-02 11:46:50 +0200

    Add files via upload

---
+++ GameInTime/GameInTime/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.UbuntuTheme1 = New GameInTime.UbuntuTheme()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.UbuntuTextBox1 = New GameInTime.UbuntuTextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.UbuntuButtonOrange1 = New GameInTime.UbuntuButtonOrange()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.UbuntuTheme1.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'UbuntuTheme1
+        '
+        Me.UbuntuTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.UbuntuTheme1.Controls.Add(Me.ProgressBar1)
+        Me.UbuntuTheme1.Controls.Add(Me.Panel3)
+        Me.UbuntuTheme1.Controls.Add(Me.TabControl1)
+        Me.UbuntuTheme1.Controls.Add(Me.Label1)
+        Me.UbuntuTheme1.Controls.Add(Me.ListBox3)
+        Me.UbuntuTheme1.Controls.Add(Me.UbuntuTextBox1)
+        Me.UbuntuTheme1.Controls.Add(Me.WebBrowser1)
+        Me.UbuntuTheme1.Controls.Add(Me.PictureBox1)
+        Me.UbuntuTheme1.Controls.Add(Me.UbuntuButtonOrange1)
+        Me.UbuntuTheme1.Controls.Add(Me.ListBox1)
+        Me.UbuntuTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.UbuntuTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.UbuntuTheme1.Name = "UbuntuTheme1"
+        Me.UbuntuTheme1.Size = New System.Drawing.Size(552, 470)
+        Me.UbuntuTheme1.TabIndex = 0
+        Me.UbuntuTheme1.Text = "UbuntuTheme1"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(318, 48)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(36, 23)
+        Me.ProgressBar1.TabIndex = 7
+        Me.ProgressBar1.Visible = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Silver
+        Me.Panel3.Controls.Add(Me.PictureBox2)
+        Me.Panel3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Panel3.Location = New System.Drawing.Point(4, 454)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(379, 10)
+        Me.Panel3.TabIndex = 19
+        Me.Panel3.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Coral
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(1, 10)
+        Me.PictureBox2.TabIndex = 10
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 66)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(283, 382)
+        Me.TabControl1.TabIndex = 11
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.Transparent
+        Me.TabPage1.Controls.Add(Me.ListBox2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(275, 356)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "PirateBay (top 100) Games"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(3, 3)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(269, 350)
+        Me.ListBox2.TabIndex = 6
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.ListBox4)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(275, 356)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "MyFavourite Games "
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'ListBox4
+        '
+        Me.ListBox4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Items.AddRange(New Object() {"Assassins Creed II ", "Assassins Creed I", "Assassins Creed Black Flag", "The Witcher II", "GTA San Andreas ", "Far Cry 3", "Far Cry 4", "Speedrunners", "FrostPunk"})
+        Me.ListBox4.Location = New System.Drawing.Point(3, 3)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(269, 350)
+        Me.ListBox4.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(389, 447)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(433, 145)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(74, 30)
+        Me.ListBox3.TabIndex = 8
+        Me.ListBox3.Visible = False
+        '
+        'UbuntuTextBox1
+        '
+        Me.UbuntuTextBox1.BackColor = System.Drawing.Color.White
+        Me.UbuntuTextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer), CType(CType(102, Byte), Integer))
+        Me.UbuntuTextBox1.Location = New System.Drawing.Point(3, 34)
+        Me.UbuntuTextBox1.MaxLength = 32767
+        Me.UbuntuTextBox1.Name = "UbuntuTextBox1"
+        Me.UbuntuTextBox1.Size = New System.Drawing.Size(164, 26)
+        Me.UbuntuTextBox1.TabIndex = 7
+        Me.UbuntuTextBox1.Text = "Search.."
+        Me.UbuntuTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.UbuntuTextBox1.UseSystemPasswordChar = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(419, 103)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(50, 26)
+        Me.WebBrowser1.TabIndex = 5
+        Me.WebBrowser1.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(289, 88)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(255, 356)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'UbuntuButtonOrange1
+        '
+        Me.UbuntuButtonOrange1.BackColor = System.Drawing.Color.Transparent
+        Me.UbuntuButtonOrange1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(86, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.UbuntuButtonOrange1.Location = New System.Drawing.Point(171, 34)
+        Me.UbuntuButtonOrange1.Name = "UbuntuButtonOrange1"
+        Me.UbuntuButtonOrange1.Size = New System.Drawing.Size(112, 23)
+        Me.UbuntuButtonOrange1.TabIndex = 2
+        Me.UbuntuButtonOrange1.Text = "Install"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(475, 112)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(21, 17)
+        Me.ListBox1.TabIndex = 0
+        Me.ListBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 470)
+        Me.Controls.Add(Me.UbuntuTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.UbuntuTheme1.ResumeLayout(False)
+        Me.UbuntuTheme1.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents UbuntuTheme1 As GameInTime.UbuntuTheme
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents UbuntuButtonOrange1 As GameInTime.UbuntuButtonOrange
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents UbuntuTextBox1 As GameInTime.UbuntuTextBox
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ListBox4 As System.Windows.Forms.ListBox
+
+End Class
---
+++ GameInTime/GameInTime/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+    Private Property pageready As Boolean = False
+#Region "Page Loading Functions"
+    Private Sub WaitForPageLoad()
+        AddHandler WebBrowser1.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf PageWaiter)
+        While Not pageready
+            Application.DoEvents()
+        End While
+        pageready = False
+    End Sub
+
+    Private Sub PageWaiter(ByVal sender As Object, ByVal e As WebBrowserDocumentCompletedEventArgs)
+        If WebBrowser1.ReadyState = WebBrowserReadyState.Complete Then
+            pageready = True
+            RemoveHandler WebBrowser1.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf PageWaiter)
+        End If
+    End Sub
+
+#End Region
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+    
+    Private Sub UbuntuTheme1_Click(sender As Object, e As EventArgs) Handles UbuntuTheme1.Click
+        
+    End Sub
+    Private Sub FindMyString()
+        ' Ensure we have a proper string to search for.
+        If UbuntuTextBox1.Text <> String.Empty Then
+            ' Find the item in the list and store the index to the item.
+            Dim index As Integer = ListBox2.FindString(UbuntuTextBox1.Text)
+            ' Determine if a valid index is returned. Select the item if it is valid.
+            If index <> -1 Then
+                ListBox2.SetSelected(index, True)
+            Else
+                MessageBox.Show("The search string did not match any items in the ListBox")
+            End If
+        End If
+    End Sub
+   
+    Dim items() As String = {}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        WebBrowser1.ScriptErrorsSuppressed = True
+        WebBrowser1.Navigate("https://thepiratebay.org/top/401")
+        WaitForPageLoad()
+        For Each elem As HtmlElement In WebBrowser1.Document.All
+            If elem.GetAttribute("title").Contains("Λεπτομέρειες για") Then
+
+                ListBox3.Items.Add(elem.GetAttribute("innerText"))
+                ListBox2.Items.Add(elem.GetAttribute("innerText"))
+                ReDim Preserve items(items.Length)
+                items(items.Length - 1) = elem.GetAttribute("innerText")
+
+
+            End If
+        Next
+        For Each elem As HtmlElement In WebBrowser1.Document.All
+            If elem.GetAttribute("title").Contains("Download this torrent using magnet") Then
+
+
+                ListBox1.Items.Add(elem.GetAttribute("href"))
+
+
+            End If
+        Next
+    End Sub
+
+    Private Sub UbuntuButtonOrange1_Click(sender As Object, e As EventArgs) Handles UbuntuButtonOrange1.Click
+
+        If (ListBox2.SelectedItem <> "") Then
+            Label1.Text = ListBox2.SelectedItem.ToString
+            For x As Integer = 0 To ListBox3.Items.Count - 1
+
+                If ListBox3.Items(x).ToString = ListBox2.SelectedItem.ToString Then
+                    ListBox3.SelectedIndex = x
+                End If
+            Next
+            ListBox1.SetSelected(ListBox3.SelectedIndex, True)
+            MessageBox.Show("Επιλέξατε: " & ListBox2.SelectedItem, "Pirate Bay", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            WebBrowser1.Navigate(ListBox1.SelectedItem)
+        Else
+            MessageBox.Show("You must select an item", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub UbuntuButtonOrange2_Click(sender As Object, e As EventArgs)
+
+
+
+    End Sub
+
+    Private Sub UbuntuTextBox1_Click(sender As Object, e As EventArgs) Handles UbuntuTextBox1.Click
+
+    End Sub
+
+    Private Sub UbuntuTextBox1_TextChanged(sender As Object, e As EventArgs) Handles UbuntuTextBox1.TextChanged
+        If UbuntuTextBox1.Text = "" Then
+            ListBox2.Items.Clear()
+            ListBox2.Items.AddRange(ListBox3.Items)
+
+        End If
+
+        If ListBox2.Items.Count = 0 Then
+            ListBox2.Items.AddRange(ListBox3.Items)
+        End If
+
+        Dim items = From it In ListBox2.Items.Cast(Of Object)()
+                Where it.ToString().IndexOf(UbuntuTextBox1.Text, StringComparison.CurrentCultureIgnoreCase) >= 0
+        Dim matchingItemList As List(Of Object) = items.ToList()
+        ListBox2.BeginUpdate()
+        ListBox2.Items.Clear()
+        For Each item In matchingItemList
+            ListBox2.Items.Add(item)
+        Next
+        ListBox2.EndUpdate()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(1)
+        Label1.Text = ProgressBar1.Value & "%" & " Proccessed."
+        PictureBox2.Width = CInt(ProgressBar1.Value * 3.79)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        PictureBox2.Visible = True
+        Panel3.Visible = True
+        Timer1.Start()
+    End Sub
+End Class
